c26584f27d0aa9ef610ebf930eb649a7a16f2845
mhardiansyah <mhardiansyah5766@gmail.com>
2025-07-18 14:11:33 +0700

    ni login

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,9 +1,66 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sakusantri/app/routes/app_pages.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  // Reactive variables for email and password
+  var email = ''.obs;
+  var password = ''.obs;
 
-  final count = 0.obs;
+  // Reactive variables for error messages
+  var emailError = RxnString();
+  var passwordError = RxnString();
+
+  // Reactive variable for password visibility
+  var isPasswordHidden = true.obs;
+
+  void togglePasswordVisibility() {
+    isPasswordHidden.value = !isPasswordHidden.value;
+  }
+
+  bool validateForm() {
+    bool isValid = true;
+
+    // Reset error messages
+    emailError.value = null;
+    passwordError.value = null;
+
+    if (email.value.isEmpty) {
+      emailError.value = 'Email tidak boleh kosong';
+      isValid = false;
+    } else if (!GetUtils.isEmail(email.value)) {
+      emailError.value = 'Format email tidak valid';
+      isValid = false;
+    }
+    if (password.value.isEmpty) {
+      passwordError.value = 'Password tidak boleh kosong';
+      isValid = false;
+    } else if (password.value.length < 8) {
+      passwordError.value = 'Password minimal 8 karakter';
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  void clearForm() {
+    email.value = '';
+    password.value = '';
+    emailError.value = null;
+    passwordError.value = null;
+  }
+
+  void login() {
+    if (validateForm()) {
+      // Perform login logic here
+      Get.snackbar('Login', 'Login successful');
+      Get.toNamed(Routes.HOME);
+      clearForm();
+    } else {
+      Get.snackbar('Error', 'Please fix the errors in the form');
+    }
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -18,6 +75,4 @@ class LoginController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,22 +1,122 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:sakusantri/app/routes/app_pages.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-   LoginView({super.key});
+  LoginView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:  Text('LoginView'),
-        centerTitle: true,
-      ),
-      body:  Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 70),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/icons/SMK MQ.png', width: 100, height: 100),
+              const SizedBox(height: 20),
+              Text(
+                'Log In',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 11),
+              Text(
+                "Welcome Back to MySaku!",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(height: 40),
+              Obx(
+                () => TextField(
+                  onChanged: (value) => controller.email.value = value,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF949494)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: 'Email',
+                    hintText: "Enter your email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    errorText: controller.emailError.value,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Obx(
+                () => TextField(
+                  onChanged: (value) => controller.password.value = value,
+                  obscureText: controller.isPasswordHidden.value,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF949494)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelText: 'Password',
+                    hintText: "Enter your password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    errorText: controller.passwordError.value,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        controller.isPasswordHidden.value
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: controller.togglePasswordVisibility,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: null,
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: controller.login,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  "Log In",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account? "),
+                  InkWell(
+                    onTap: () => Get.toNamed(Routes.REGISTER),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
